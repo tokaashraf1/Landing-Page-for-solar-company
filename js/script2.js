fd764edcc00f0b0c0c1a0e7f93fec596ce818704
tokaashraf1 <122158105+tokaashraf1@users.noreply.github.com>
2025-02-03 17:08:10 +0200

    removing extra files

--- js/script2.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const hamburger = document.querySelector(".hamburger");
-  const mobileMenu = document.querySelector(".mobile-menu");
-
-  hamburger.addEventListener("click", function () {
-      mobileMenu.classList.toggle("show");
-  });
-});
-
